4c30b9b97a5673fde31912ae140d72cd63596646
aizazulllhaq <aizazulllhaq@gmail.com>
2024-09-30 23:25:43 -0700

    Ketalon Test Completed

--- cypress/e2e/Tests/KetalonTest.cy.js
+++ cypress/e2e/Tests/KetalonTest.cy.js
@@ -3,18 +3,28 @@
 const Ketalon = require("../pages/ketalonAppObjects.cy");
 const AuthObject = new Ketalon();
 
-
 describe("Ketalon Cura App Tests", () => {
-  beforeEach(() => {
-    cy.visit("https://katalon-demo-cura.herokuapp.com/profile.php#login");
-  });
+  it("Visit App, Click Make Appointment, and Authenticate", () => {
+    // Visit the app
+    cy.visit("https://katalon-demo-cura.herokuapp.com");
 
-  it("Authenticate User", () => {
+    // Click on the "Make Appointment" button
+    cy.get("#btn-make-appointment").click();
+
+    // Authenticate the user
     AuthObject.Authenticate();
-  });
 
-  it('Make Appoitment', () => {
-    
+    // Make the Appointment
+    cy.get("#combo_facility").select("Hongkong CURA Healthcare Center");
+    cy.get("#chk_hospotal_readmission").click();
+    cy.get(".col-sm-4 > :nth-child(2)").click();
+    cy.get("#txt_visit_date").type("11/09/2024");
+    cy.get("#txt_comment").click({ force: true }).type("Dr here on evening");
+    cy.get("#btn-book-appointment").click();
+
+    // Verify the Appointment
+    cy.get('h2').contains("Appointment Confirmation");
+    cy.wait(5000);
+    cy.get(".text-center > .btn").click();
   });
-  
 });
